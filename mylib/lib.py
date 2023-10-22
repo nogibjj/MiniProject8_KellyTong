26cf4d4caa66f2408083949d31f83b4afa0f8c3e
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 19:59:34 -0400

    Update lib.py

--- mylib/lib.py
+++ mylib/lib.py
@@ -2,4 +2,8 @@ def compute_average(df):
   overall_avg = df.mean().mean()
   column_avg = df.mean()
   row_avg = df.mean(axis=5)
-  return {'overall average': overall_avg, 'column average': column_avg, 'row average': row_avg}
+  return {
+          'overall average': overall_avg, 
+          'column average': column_avg, 
+          'row average': row_avg
+          }

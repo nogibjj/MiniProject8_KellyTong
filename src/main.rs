86e1b7f1415f4337daed2a2bc4d4b6d8434a5ebb
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 17:42:00 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
+// main.rs
+extern crate polars;
 
+use polars::prelude::*;
+use std::error::Error;
+
+mod mylib; // import the mylib module
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let df = CsvReader::from_path("Auto.csv")?.infer_schema(None).has_header(true).finish()?;
+    mylib::compute_average(df)?;
+
+    Ok(())
+}

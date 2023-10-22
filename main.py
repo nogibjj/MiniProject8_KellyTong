f6084162da1a9e7f352a48d86bef8f42e7159ad3
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 18:04:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import panda as pd
 
 def main():
   df=pd.read.csv("Auto.csv")
-  compute_average(df)
+  if not df.empty:
+    print("DataFrame is not empty.")
+    avg = compute_average(df)
+    print(f"overall average: {avg['overall_avg']:.2f}")
+    print(f"column average: {avg['column_avg']:.2f}")
+    print(f"row average: {avg['row_avg']:.2f}")
+else:
+    print("DataFrame is empty.")
+
 
 if __name__ == "__main__":
     main()

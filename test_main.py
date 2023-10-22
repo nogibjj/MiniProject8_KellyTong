c337c457402913f936afc806219f130b989df010
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 18:08:07 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,12 @@
+import panda as pd
+from main import main
 
+
+def test_compute_average():
+  df = pd.read.csv("Auto.csv")
+  assert "overall average" in avg, "overall average not computed"
+  assert "column average" in avg, "column average not computed"
+  assert "row average" in avg, "row average not computed"
+
+if __name__ == "__main__":
+    test_compute_average()

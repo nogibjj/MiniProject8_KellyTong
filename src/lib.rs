653cd69904aaf820ac7ca47c7f68c5c224e4265e
GitHub Action <action@github.com>
2023-10-23 01:33:18 +0000

    Add metric log

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,25 @@ use polars::prelude::*;
 use std::collections::HashMap;
 use std::result::Result as StdResult;
 
-pub fn compute_average(df: &DataFrame) -> StdResult<HashMap<String, f64>, polars::error::PolarsError> {
+pub fn compute_average(
+    df: &DataFrame,
+) -> StdResult<HashMap<String, f64>, polars::error::PolarsError> {
     let average_mpg = df
         .column("mpg")
         .map_err(|_| polars::error::PolarsError::NotFound("mpg column not found".into()))?
         .mean()
-        .ok_or_else(|| polars::error::PolarsError::NotFound("Failed to compute mean for mpg".into()))?;
+        .ok_or_else(|| {
+            polars::error::PolarsError::NotFound("Failed to compute mean for mpg".into())
+        })?;
 
     let average_weight = df
         .column("weight")
         .map_err(|_| polars::error::PolarsError::NotFound("weight column not found".into()))?
         .mean()
-        .ok_or_else(|| polars::error::PolarsError::NotFound("Failed to compute mean for weight".into()))?;
-    
+        .ok_or_else(|| {
+            polars::error::PolarsError::NotFound("Failed to compute mean for weight".into())
+        })?;
+
     let mut averages = HashMap::new();
     averages.insert("mpg average".to_string(), average_mpg);
     averages.insert("weight average".to_string(), average_weight);
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,27 @@ mod lib;
 
 fn main() -> StdResult<(), Box<dyn Error>> {
     let file_path = "Auto.csv";
-    
-    let df = CsvReader::from_path(file_path)?.infer_schema(None).has_header(true).finish()?;
+
+    let df = CsvReader::from_path(file_path)?
+        .infer_schema(None)
+        .has_header(true)
+        .finish()?;
 
     if !df.is_empty() {
         println!("DataFrame is not empty.");
 
         let avg = lib::compute_average(&df)?;
-        match (avg.get("overall average"), avg.get("column average"), avg.get("row average")) {
+        match (
+            avg.get("overall average"),
+            avg.get("column average"),
+            avg.get("row average"),
+        ) {
             (Some(overall_avg), Some(column_avg), Some(row_avg)) => {
                 println!("overall average: {:.2}", overall_avg);
                 println!("column average: {:.2}", column_avg);
                 println!("row average: {:.2}", row_avg);
-            },
-            _ => println!("Failed to compute some averages.")
+            }
+            _ => println!("Failed to compute some averages."),
         }
     } else {
         println!("DataFrame is empty.");

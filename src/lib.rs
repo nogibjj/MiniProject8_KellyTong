2c381fafedaa367a4ba168b2efe5f1579f68d651
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 20:42:27 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,26 @@
-extern crate ndarray;
-use ndarray::{Array2, Axis};
+use polars::prelude::*;
 use std::collections::HashMap;
 
-pub fn compute_average(df: &Array2<f64>) -> Result<HashMap<String, f64>, &'static str> {
-    // Select only numeric columns - since we are using ndarray and assuming f64, this step is skipped
+pub fn compute_average(df: &DataFrame) -> Result<HashMap<String, f64>, polars::error::PolarsError> {
+    let average_mpg = df.column("mpg")?
+        .mean()?
+        .get(0)
+        .and_then(|v| v.get().ok_or_else(|| polars::error::PolarsError::ComputeError("Mean computation failed".into())))
+        .map(|v| v.to_f64());
 
-    let overall_avg = df.mean().ok_or("Failed to compute overall average")?;
-    
-    // Compute mean of each column
-    let column_avg = df.mean_axis(Axis(0)).ok_or("Failed to compute column average")?;
-    
-    // Compute mean of each row
-    let row_avg = df.mean_axis(Axis(1)).ok_or("Failed to compute row average")?;
-    
-    // We'll use a HashMap to return the results similar to a dictionary in Python
-    let mut result = HashMap::new();
-    result.insert("overall average".to_string(), overall_avg);
-    result.insert("column average".to_string(), column_avg.mean().ok_or("Failed to compute column average mean")?);
-    result.insert("row average".to_string(), row_avg.mean().ok_or("Failed to compute row average mean")?);
-    
-    Ok(result)
+    let average_weight = df.column("weight")?
+        .mean()?
+        .get(0)
+        .and_then(|v| v.get().ok_or_else(|| polars::error::PolarsError::ComputeError("Mean computation failed".into())))
+        .map(|v| v.to_f64());
+
+    match (average_mpg, average_weight) {
+        (Some(mpg), Some(weight)) => {
+            let mut map = HashMap::new();
+            map.insert("weight average".to_string(), weight);
+            map.insert("mpg average".to_string(), mpg);
+            Ok(map)
+        }
+        _ => Err(polars::error::PolarsError::ComputeError("Failed to compute averages".into()))
+    }
 }

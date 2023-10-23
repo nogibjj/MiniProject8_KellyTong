3ab25d1ed5460ce90ca3a1bfc5e05865fa6af578
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 20:54:58 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,19 @@ use std::collections::HashMap;
 use std::result::Result as StdResult;
 
 pub fn compute_average(df: &DataFrame) -> StdResult<HashMap<String, f64>, polars::error::PolarsError> {
-    let average_mpg = df.column("mpg")?
-        .mean()?
-        .get(0)
-        .and_then(|v| v.get().ok_or_else(|| polars::error::PolarsError::ComputeError("Mean computation failed".into())))
-        .map(|v| v.to_f64());
+    let average_mpg = df
+        .column("mpg")
+        .ok_or(polars::error::PolarsError::NotFound("mpg column not found".into()))?
+        .mean()?;
 
-    let average_weight = df.column("weight")?
-        .mean()?
-        .get(0)
-        .and_then(|v| v.get().ok_or_else(|| polars::error::PolarsError::ComputeError("Mean computation failed".into())))
-        .map(|v| v.to_f64());
+    let average_weight = df
+        .column("weight")
+        .ok_or(polars::error::PolarsError::NotFound("weight column not found".into()))?
+        .mean()?;
 
-    match (average_mpg, average_weight) {
-        (Some(mpg), Some(weight)) => {
-            let mut map = HashMap::new();
-            map.insert("weight average".to_string(), weight);
-            map.insert("mpg average".to_string(), mpg);
-            Ok(map)
-        }
-        _ => Err(polars::error::PolarsError::ComputeError("Failed to compute averages".into()))
-    }
+    let mut averages = HashMap::new();
+    averages.insert("mpg average".to_string(), average_mpg);
+    averages.insert("weight average".to_string(), average_weight);
+
+    Ok(averages)
 }

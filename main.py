e4a4d8916227f5ef3860e5fd4f7be79ee614c9b2
GitHub Action <action@github.com>
2023-10-23 00:39:49 +0000

    Add metric log

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from mylib.lib import compute_average
 import pandas as pd
 
+
 def main():
-  df=pd.read_csv("Auto.csv")
-  if not df.empty:
-    print("DataFrame is not empty.")
-    avg = compute_average(df)
-    print(f"overall average: {avg['overall_avg']:.2f}")
-    print(f"column average: {avg['column_avg']:.2f}")
-    print(f"row average: {avg['row_avg']:.2f}")
-  else:
-    print("DataFrame is empty.")
+    df = pd.read_csv("Auto.csv")
+    if not df.empty:
+        print("DataFrame is not empty.")
+        avg = compute_average(df)
+        print(f"overall average: {avg['overall_avg']:.2f}")
+        print(f"column average: {avg['column_avg']:.2f}")
+        print(f"row average: {avg['row_avg']:.2f}")
+    else:
+        print("DataFrame is empty.")
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -3,10 +3,11 @@ from main import compute_average
 
 
 def test_compute_average():
-  df = pd.read_csv("Auto.csv")
-  avg = compute_average(df)
-  assert "weight average" in avg, "weight average not computed"
-  assert "mpg average" in avg, "mpg average not computed"
+    df = pd.read_csv("Auto.csv")
+    avg = compute_average(df)
+    assert "weight average" in avg, "weight average not computed"
+    assert "mpg average" in avg, "mpg average not computed"
+
 
 if __name__ == "__main__":
     test_compute_average()

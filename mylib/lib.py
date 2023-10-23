58ef3e9c3ee4e0394d5b4d00cf36fa45c3a1fbf0
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 20:30:05 -0400

    Update lib.py

--- mylib/lib.py
+++ mylib/lib.py
@@ -1,14 +1,9 @@
 import numpy as np
 
 def compute_average(df):
-  # Select only numeric columns
-  numeric_df = df.select_dtypes(include=[np.number])
-  # Compute the mean of the numeric columns
-  overall_avg = numeric_df.mean().mean()
-  column_avg = df.mean()
   row_avg = df.mean(axis=5)
+  row_avg2 = df.mean(axis=1)
   return {
-          'overall average': overall_avg, 
-          'column average': column_avg, 
-          'row average': row_avg
+          'weight average': row_avg,
+          'mpg average': row_avg2
           }

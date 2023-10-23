289f46ff3f93def9e42292632d17441fc9163bd0
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 21:11:04 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ extern crate polars;
 use polars::prelude::*;
 use std::collections::HashMap;
 use std::error::Error;
+use std::result::Result as StdResult;
 
 mod lib;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> StdResult<(), Box<dyn Error>> {
     let file_path = "Auto.csv";
     let df = DataFrame::read_csv(file_path, CsvReader::default())?;
 
     if !df.is_empty() {
         println!("DataFrame is not empty.");
 
-        let avg = mylib::compute_average(&df)?;
+        let avg = lib::compute_average(&df)?;
         match (avg.get("overall average"), avg.get("column average"), avg.get("row average")) {
             (Some(overall_avg), Some(column_avg), Some(row_avg)) => {
                 println!("overall average: {:.2}", overall_avg);

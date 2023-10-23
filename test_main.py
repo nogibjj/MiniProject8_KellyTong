40bc1dd4d713f79995d27b91d1b9f4c07d7d8262
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-22 20:30:40 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,9 +5,8 @@ from main import compute_average
 def test_compute_average():
   df = pd.read_csv("Auto.csv")
   avg = compute_average(df)
-  assert "overall average" in avg, "overall average not computed"
-  assert "column average" in avg, "column average not computed"
-  assert "row average" in avg, "row average not computed"
+  assert "weight average" in avg, "weight average not computed"
+  assert "mpg average" in avg, "mpg average not computed"
 
 if __name__ == "__main__":
     test_compute_average()

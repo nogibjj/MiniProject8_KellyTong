e04458bfb5e59aeea5cf9959a2d39d7124733e8d
GitHub Action <action@github.com>
2023-10-24 04:01:53 +0000

    Add metric log

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import psutil
 # Record the start time
 start_time = time.time()
 
+
 def main():
     df = pd.read_csv("Auto.csv")
     if not df.empty:
@@ -23,11 +24,11 @@ def main():
     elapsed_time = end_time - start_time
     cpu_percent = psutil.cpu_percent()
     memory_info = psutil.virtual_memory()
-    
+
     print(f"Elapsed time: {elapsed_time:.4f} seconds")
     print(f"CPU Usage: {cpu_percent}%")
     print(f"Memory Usage: {memory_info.percent}%")
 
+
 if __name__ == "__main__":
     main()
-    

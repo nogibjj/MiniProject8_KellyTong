6c09fd35effa7cfc673e3746a61bd70bd8f1244d
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-23 23:51:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from mylib.lib import compute_average
 import pandas as pd
+import time
+import psutil
 
+# Record the start time
+start_time = time.time()
 
 def main():
     df = pd.read_csv("Auto.csv")
@@ -16,3 +20,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+    end_time = time.time()
+
+# Calculate the elapsed time
+elapsed_time = end_time - start_time
+cpu_percent = psutil.cpu_percent()
+memory_info = psutil.virtual_memory()
+
+print(f"Elapsed time: {elapsed_time:.4f} seconds")
+print(f"CPU Usage: {cpu_percent}%")
+print(f"Memory Usage: {memory_info.percent}%")

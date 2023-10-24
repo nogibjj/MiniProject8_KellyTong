2a71d12dc68e0e76731f3aa1419a730b711bde30
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-23 23:56:10 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::result::Result as StdResult;
 mod lib;
 
 fn main() -> StdResult<(), Box<dyn Error>> {
+    let start_time = Instant::now();
+    
     let file_path = "Auto.csv";
 
     let df = CsvReader::from_path(file_path)?
@@ -33,6 +35,40 @@ fn main() -> StdResult<(), Box<dyn Error>> {
     } else {
         println!("DataFrame is empty.");
     }
+    
+    // Calculate the elapsed time and resource usage
+    let elapsed_time = end_time.duration_since(start_time);
+    println!("Total execution time: {:?}", elapsed_time); // Print the elapsed time
+                                                          // Memory usage
+    let mem_info = sys_info::mem_info().unwrap();
+    println!(
+        "Memory Usage: {}%",
+        (mem_info.total - mem_info.avail) as f32 / mem_info.total as f32 * 100.0
+    );
+    // CPU calculation
+    let output = Command::new("ps")
+        .arg("-o")
+        .arg("%cpu")
+        .arg("-p")
+        .arg(format!("{}", std::process::id()))
+        .output()
+        .expect("Failed to execute ps command");
 
+    // Convert the output to a string
+    let usage = String::from_utf8_lossy(&output.stdout);
+    let lines: Vec<&str> = usage.split('\n').collect();
+
+    // Parse the CPU usage from the output
+    if lines.len() >= 2 {
+        let usage_str = lines[1].trim();
+        let usage_float: Result<f32, _> = usage_str.parse();
+        match usage_float {
+            Ok(usage) => println!("CPU Usage: {:.2}%", usage),
+            Err(_) => println!("Failed to parse CPU usage"),
+        }
+    } else {
+        println!("Failed to get CPU usage");
+    }
+    
     Ok(())
 }
